1025f4ddf91f85ff41f82d7a598ba2389f8a8339
electric-lost-lamb <48010974+electric-lost-lamb@users.noreply.github.com>
2019-03-06 13:18:31 +0900

    Update config.js

--- demo/js/config.js
+++ demo/js/config.js
@@ -11,7 +11,7 @@ var config = {
   // If Github mode is set, your Github username, repo name, 
   // and branch to get files from. 
   github: {
-    username: 'chrisdiana',
+    username: 'electric-lost-lamb',
     repo: 'cms.js',
     branch: 'gh-pages',
     host: 'https://api.github.com',
